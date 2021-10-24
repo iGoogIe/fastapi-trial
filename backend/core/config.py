13451c509395fe3174a0b3a808e2ec418a16f0f4
Jey Farmer <jeyfarmer@hotmail.com>
2021-10-23 23:05:45 -0500

    env configs and db connection

---
+++ backend/core/config.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# This means go to current dir (backend folder) and load up the .env file
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+class Settings:
+    PROJECT_TITLE: str = "Jobboard"
+    PROJECT_VERSION: str = "0.0.1"
+    HOST: str = "0.0.0.0"
+    PORT: int = 8000
+
+    POSTGRES_USER: str = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD: str = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER")
+    POSTGRES_DB: str = os.getenv("POSTGRES_DB")
+    POSTGRES_PORT: int = os.getenv("POSTGRES_PORT")
+    DATABASE_URL: str = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    
+
+settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,30 @@
 from fastapi import FastAPI
-from config import settings
+from core.config import settings
+from db.session import engine
+from db.base_class import Base
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app = FastAPI(
+        title=settings.PROJECT_TITLE,
+        version=settings.PROJECT_VERSION,
+        host=settings.HOST,
+        port=settings.PORT,
+        reload=True
+    )
+
+    create_tables()
+
+    return app
 
 # set 0.0.0.0 IP address to be accessible from outside
 
 
-app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION, host=settings.HOST, port=settings.PORT, reload=True)
+# app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION, host=settings.HOST, port=settings.PORT, reload=True)
 
+app = start_application()
 
 @app.get("/")
 def hello_api():
